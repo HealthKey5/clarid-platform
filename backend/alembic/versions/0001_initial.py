2983d29e61a86b495deb681654d86001375f219a
ClarID <brandonkingdo@gmail.com>
2026-05-20 14:21:53 -0500

    chore: add initial Alembic migration

---
+++ backend/alembic/versions/0001_initial.py
@@ -0,0 +1,23 @@
+"""initial
+
+Revision ID: 0001_initial
+Revises: 
+Create Date: 2026-05-20
+"""
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "0001_initial"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+
+def downgrade() -> None:
+    pass
